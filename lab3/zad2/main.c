6f1e5f62e0152c54c0c904fa7c84df10e0c2e818
iwoplaza <iwoplaza@gmail.com>
2021-03-29 10:10:07 +0200

    Implemented the second exercise

--- lab3/zad2/main.c
+++ lab3/zad2/main.c
@@ -3,16 +3,23 @@
 #include <time.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <wait.h>
+#include <stdlib.h>
+#include <sys/times.h>
 
-void print_time(FILE* report_file, const char* label, int lines, clock_t delta)
+//#define WRITE_TO_FILE
+
+void print_time(FILE* report_file, const char* label, clock_t clock_delta, clock_t user_delta, clock_t system_delta)
 {
-    double miliseconds = (double) delta / CLOCKS_PER_SEC * 1000;
-    printf("TIME: %-20s %10ld ticks, %10f ms\n", label, delta, miliseconds);
+    long clocks_per_sec = sysconf(_SC_CLK_TCK);
+    double clock_ms = (double) clock_delta / clocks_per_sec * 1000;
+    double user_ms = (double) user_delta / clocks_per_sec * 1000;
+    double system_ms = (double) system_delta / clocks_per_sec * 1000;
+    printf("TIME: %-20s (real) %10f ms, (user) %10f ms, (system) %10f ms\n", label, clock_ms, user_ms, system_ms);
 
-    char buffer[256];
-    sprintf(buffer, "%s (%d lines)", label, lines);
-
-    fprintf(report_file, "%-32s %10ld ticks, %10f ms\n", buffer, delta, miliseconds);
+    fprintf(report_file, "%-32s (real) %10f ms, (user) %10f ms, (system) %10f ms\n",
+            label, clock_ms, user_ms, system_ms);
 }
 
 bool split_pair(char* buffer, char** filename_a, char** filename_b)
@@ -33,8 +40,11 @@ bool split_pair(char* buffer, char** filename_a, char** filename_b)
     return true;
 }
 
-bool merge_pair(MUMergeTable_t* table, const char* filename_a, const char* filename_b)
+bool merge_pair(const char* filename_a, const char* filename_b)
 {
+    // Creating a seperate merge table for each pair, so that we don't copy the heap each time we create a process.
+    MUMergeTable_t* table = mu_init_merge_table();
+
     int merge_status = mu_merge_files(table, filename_a, filename_b);
     if (merge_status != 0)
     {
@@ -42,17 +52,25 @@ bool merge_pair(MUMergeTable_t* table, const char* filename_a, const char* filen
         return false;
     }
 
+    #ifdef WRITE_TO_FILE
+    char out_filename[256];
+    sprintf(out_filename, "%d.txt", getpid());
+    mu_write_block_to_file(table, mu_get_num_of_blocks(table) - 1, out_filename);
+    #endif
+
+    mu_free_table(table);
     return true;
 }
 
-bool do_merge_files(FILE* report_file, MUMergeTable_t* table)
+bool do_merge_files(FILE* report_file)
 {
     // Reading the amount of pairs
     int amount_of_pairs;
     scanf("%d", &amount_of_pairs);
 
     // Starting to measure time
-    clock_t start = clock();
+    struct tms start;
+    clock_t start_clock = times(&start);
 
     char buffer[256];
     for (int i = 0; i < amount_of_pairs; ++i)
@@ -69,15 +87,40 @@ bool do_merge_files(FILE* report_file, MUMergeTable_t* table)
 
         printf("a: %s, b: %s\n", filename_a, filename_b);
 
-        if (!merge_pair(table, filename_a, filename_b))
+        pid_t pid = fork();
+
+        if (pid == 0)
         {
-            return false;
+            if (!merge_pair(filename_a, filename_b))
+            {
+                exit(1);
+            }
+            // We don't want the child process to do any more work.
+            exit(0);
         }
     }
 
-    clock_t delta = clock() - start;
-    int num_of_rows = mu_get_num_of_rows(table, mu_get_num_of_blocks(table) - 1);
-    print_time(report_file, "merge_files", num_of_rows, delta);
+    // Waiting for each child process to end.
+    pid_t child_pid;
+    int status = 0;
+    while ((child_pid = wait(&status)) > 0)
+    {
+        if (status == 0)
+        {
+            printf("Child process %d ended with a SUCCESS.\n", child_pid);
+        }
+        else
+        {
+            fprintf(stderr, "Child process %d ended with a FAILURE.\n", child_pid);
+        }
+    }
+
+    struct tms end;
+    clock_t end_clock = times(&end);
+    print_time(report_file, "merge_files",
+               end_clock - start_clock,
+               end.tms_cutime - start.tms_cutime,
+               end.tms_cstime - start.tms_cstime);
 
     return true;
 }
@@ -91,13 +134,10 @@ int main()
         return 1;
     }
 
-    MUMergeTable_t* table = mu_init_merge_table();
-
     // Performing the merge
-    do_merge_files(report_file, table);
+    do_merge_files(report_file);
 
     // Freeing the memory.
-    mu_free_table(table);
     fclose(report_file);
 
     return 0;
